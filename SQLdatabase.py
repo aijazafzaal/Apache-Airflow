f17453030aee9cdaedbd357c5d01fcd2ec6c3274
aijazafzaal <35755621+aijazafzaal@users.noreply.github.com>
2023-01-26 21:07:19 +0100

    Add files via upload

---
+++ SQLdatabase.py
@@ -0,0 +1,33 @@
+import requests
+import xmltodict
+
+from airflow.decorators import dag, task
+import pendulum
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+
+PODCAST_URL = "https://www.marketplace.org/feed/podcast/marketplace/"
+
+@dag(
+    dag_id='podcast_summary',
+    schedule_interval="@daily",
+    start_date=pendulum.datetime(2022, 5, 30),
+    catchup=False,
+)
+def podcast_summary():
+    create_database = SqliteOperator(
+        task_id='create_table_sqlite',
+        sqlite_conn_id="podcasts"
+    )
+
+    @task()
+    def get_episodes():
+        data = requests.get(PODCAST_URL)
+        feed = xmltodict.parse(data.text)
+        episodes = feed["rss"]["channel"]["item"]
+        print(f"Found {len(episodes)} episodes.")
+        return episodes
+
+    podcast_episodes = get_episodes()
+    create_database.set_downstream(podcast_episodes)
+
+summary = podcast_summary()
